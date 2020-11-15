d643792bc16f3cd7461d2c0f49c2f372e12ac806
JNickBaughan <JNickBaughan@gmail.com>
2020-11-15 18:26:45 -0500

    the boring basics

---
+++ basics.py
@@ -0,0 +1,68 @@
+
+separator = "--" * 30
+
+# you multiply strings
+print(separator)
+
+
+num = "3"
+
+print(num)
+
+print(int(num) + 3)
+
+
+print(separator)
+
+
+name = 'Nick'
+
+print(name)
+
+
+print(separator)
+
+
+trueNumber = int(True)
+
+falseNumber = int(False)
+
+print(trueNumber)
+print(falseNumber)
+
+
+print(separator)
+
+
+printMessage = False
+if(printMessage):
+    print("perform is true")
+
+
+print(separator)
+
+
+longNumber = 1_000_000
+
+print(longNumber)
+
+
+print(separator)
+
+
+print(55 / 6)
+
+# floor division, cuts off the decimal
+print(55 // 6)
+
+# modulus operator, remainder
+print(55 % 6)
+
+
+print(separator)
+
+
+# power operator
+print(2 ** 3)
+
+    
