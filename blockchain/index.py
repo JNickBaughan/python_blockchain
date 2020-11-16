e885cf841da34e44fd250ef267f3bd1545e2b331
JNickBaughan <JNickBaughan@gmail.com>
2020-11-16 08:16:53 -0500

    init blockchain function

---
+++ blockchain/index.py
@@ -0,0 +1,37 @@
+blockchain = []
+
+def get_last_blockchain_value():
+    if len(blockchain) > 0:
+        return blockchain[-1]
+    else:
+        return [1]
+
+
+def add_value(value):
+    blockchain.append([get_last_blockchain_value(), value])
+
+
+def print_entire_blockchain():
+    print(blockchain)
+
+
+def print_block(index = 0):
+    if len(blockchain) > 0 and index <= len(blockchain)-1:
+        if index < 0:
+            print('ERROR: this function doesn\'t handle negative index')
+        else:
+            print(blockchain[index])
+    elif index > len(blockchain)-1:
+        print('ERROR: index is out of range')
+
+
+add_value(3.4)
+add_value(2)
+add_value(7.2)
+
+print_entire_blockchain()
+print_block(0)
+
+print_block(4)
+
+print_block(-1)
